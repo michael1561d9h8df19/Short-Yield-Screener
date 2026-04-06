0d4bdb10dc3c40fdbe71964c9c3867f1a9d1f1b3
michael1561d9h8df19 <michaelgslonim@gmail.com>
2026-04-06 15:10:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const STORAGE_KEY = 'gemini_api_key';
-const MODEL = 'gemini-1.5-flash';
+const MODEL = 'gemini-2.0-flash';
 
 window.onload = () => {
     const savedKey = localStorage.getItem(STORAGE_KEY);
